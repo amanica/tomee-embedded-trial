b52c6ec1989e055134816e5c5f72035b1b062ea1
Marius Kruger <amanic@gmail.com>
2014-05-19 17:26:03 +0200

    Try getting it to work with hibernate
    * use Tommee trunk
    * add Movie entity and Movies @Singleton
    * tried to put the persistance.xml everywhere I could think off but, but
    to no avail :(

---
+++ src/main/java/com/example/Movies.java
@@ -0,0 +1,54 @@
+
+package com.example;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+@Singleton
+@Startup
+@LocalBean
+public class Movies {
+
+    @PersistenceContext(unitName = "movie-unit"
+    // ,
+    // type = PersistenceContextType.EXTENDED
+    )
+    private EntityManager entityManager;
+
+    public void addMovie(Movie movie) throws Exception {
+        entityManager.persist(movie);
+    }
+
+    public void deleteMovie(Movie movie) throws Exception {
+        entityManager.remove(movie);
+    }
+
+    public List<Movie> getMovies() throws Exception {
+        Query query = entityManager.createQuery("SELECT m from Movie as m");
+        return query.getResultList();
+    }
+
+    @PostConstruct
+    public void testHibernate() throws Exception {
+        System.out.println(" ##### testHibernate() ");
+        addMovie(new Movie("Quentin Tarantino", "Reservoir Dogs", 1992));
+        addMovie(new Movie("Joel Coen", "Fargo", 1996));
+        addMovie(new Movie("Joel Coen", "The Big Lebowski", 1998));
+
+        List<Movie> list = getMovies();
+        System.out.println(" ##### List.size() " + list.size());
+
+        for (Movie movie : list) {
+            System.out.println(" ##### movie " + movie.getTitle());
+            deleteMovie(movie);
+        }
+        System.out.println(" ##### Movies.getMovies() " + getMovies().size());
+    }
+}
--- src/main/java/com/example/MyEmbededTomee.java
+++ src/main/java/com/example/MyEmbededTomee.java
@@ -1,41 +1,130 @@
 package com.example;
 
-import java.util.Properties;
+import java.io.File;
+import java.lang.reflect.Field;
 
-import javax.ejb.embeddable.EJBContainer;
-import javax.naming.Context;
+import org.apache.tomee.embedded.Configuration;
+import org.apache.tomee.embedded.Container;
 
 public class MyEmbededTomee {
+    // @Parameter(defaultValue = "${project.packaging}")
+    protected String packaging = "war";
+
+    // @Parameter(
+    // defaultValue = "${project.build.directory}/${project.build.finalName}")
+    protected File warFile;
+
+    // @Parameter(property = "tomee-embedded-plugin.http", defaultValue =
+    // "8080")
+    private int httpPort;
+
+    // @Parameter(property = "tomee-embedded-plugin.ajp", defaultValue = "8009")
+    private int ajpPort = 8009;
+
+    // @Parameter(property = "tomee-embedded-plugin.stop", defaultValue =
+    // "8005")
+    private int stopPort;
+
+    // @Parameter(property = "tomee-embedded-plugin.host",
+    // defaultValue = "localhost")
+    private String host;
+
+    // @Parameter(property = "tomee-embedded-plugin.lib",
+    // defaultValue = "${project.build.directory}/apache-tomee-embedded")
+    protected String dir;
+
+    // @Parameter
+    private File serverXml;
+
     public static void main(String[] args) throws Exception {
+        new MyEmbededTomee().run();
+    }
 
-        Properties p = new Properties();
+    private void run() {
+        // Properties p = new Properties();
+        //
+        // p.setProperty(EJBContainer.PROVIDER, "tomee-embedded");
+        // p.setProperty(
+        // "javax.ejb.embeddable.modules",
+        // "/stf/prj/tmp/tomee-embedded-trial/target/"
+        // + "tomee-embedded-trial-0.0.1-SNAPSHOT");
+        //
+        // p.put("movieDatabase", "new://Resource?type=DataSource");
+        // p.put("movieDatabase.JdbcDriver", "org.hsqldb.jdbcDriver");
+        // p.put("movieDatabase.JdbcUrl", "jdbc:hsqldb:mem:moviedb");
+        // p.put("openejb.validation.output.level", "VERBOSE");
+        //
+        // Context context =
+        // EJBContainer.createEJBContainer(p).getContext();
 
-        p.setProperty(EJBContainer.PROVIDER, "tomee-embedded");
-        p.setProperty(
-                "javax.ejb.embeddable.modules",
-                "/stf/prj/tmp/tomee-embedded-trial/target/"
-                    + "tomee-embedded-trial-0.0.1-SNAPSHOT");
-
-        p.put("movieDatabase", "new://Resource?type=DataSource");
-        p.put("movieDatabase.JdbcDriver", "org.hsqldb.jdbcDriver");
-        p.put("movieDatabase.JdbcUrl", "jdbc:hsqldb:mem:moviedb");
-        p.put("openejb.validation.output.level", "VERBOSE");
-
-        Context context =
-            EJBContainer.createEJBContainer(p).getContext();
-
-        System.out.println("*** started ***");
-
+        final Container container = new Container();
+        final Configuration config = getConfig();
+        container.setup(config);
         try {
-            for (int i = 0; i < 10; i++) {
-                System.out.println(" " + i);
-                Thread.sleep(20000);
+            container.start();
+
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        container.undeploy(warFile.getAbsolutePath());
+                        container.stop();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        System.out.println("can't stop TomEE");
+                    }
+                }
+            });
+
+            container.deploy(warFile.getName(), warFile);
+
+            System.out.println(
+                "TomEE embedded started on " + config.getHost() + ":"
+                    + config.getHttpPort());
+            container.await();
+            System.out.println("*** started ***");
+
+            try {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(" " + i);
+                    Thread.sleep(20000);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+
+            System.out.println("*** closing ***");
+            container.stop();
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("*** can't start TomEE");
         }
 
-        System.out.println("*** closing ***");
-        context.close();
+    }
+
+    private Configuration getConfig() { // lazy way but it works fine
+        final Configuration config = new Configuration();
+        for (Field field : getClass().getDeclaredFields()) {
+            try {
+                final Field configField =
+                    Configuration.class.getDeclaredField(field.getName());
+                field.setAccessible(true);
+                configField.setAccessible(true);
+
+                final Object value = field.get(this);
+                if (value != null) {
+                    configField.set(config, value);
+                    System.out.println("using " + field.getName() + " = "
+                        + value);
+                }
+            } catch (NoSuchFieldException nsfe) {
+                // ignored
+            } catch (Exception e) {
+                System.out.println("can't initialize attribute "
+                    + field.getName());
+            }
+
+        }
+        return config;
     }
 }
